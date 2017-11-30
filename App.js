d0c832ddbe259344ff7052471ae1e4acecaddd28
Jonahh <legiona14@gmail.com>
2017-11-30 21:46:36 +0700

    Build database

--- App.js
+++ App.js
@@ -1,16 +1,75 @@
-import React, {Component} from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
 import {
-  View,
+  Platform,
+  StyleSheet,
   Text,
-  TouchableOpacity
+  View,
+  FlatList
 } from 'react-native';
-import MenuView from './screens/MenuScreen/menuView';
 
 
-export default class App extends Component {
+
+export default class App extends Component<{}> {
+	constructor(prop){
+		super(prop);
+		this.state={
+			mang:[
+				{key:'a',hoten:'Na'},
+				{key:'b',hoten:'Nguyen'},
+				{key:'c',hoten:'Lan'},
+				{key:'d',hoten:'Sanh'},
+				{key:'c',hoten:'Lan'},
+				{key:'d',hoten:'Sanh'},
+				{key:'c',hoten:'Lan'},
+				{key:'d',hoten:'Sanh'},
+				{key:'c',hoten:'Lan'},
+				{key:'d',hoten:'Sanh'},
+				{key:'c',hoten:'Lan'},
+				{key:'d',hoten:'Sanh'},
+				{key:'c',hoten:'Lan'},
+				{key:'d',hoten:'Sanh'},
+				{key:'c',hoten:'Lan'},
+				{key:'d',hoten:'Sanh'},
+			]
+		}
+	}
   render() {
     return (
-      <MenuView/>
+      <View >
+       <FlatList
+       		data={this.state.mang}
+       		renderItem=
+       		{({item})=> 
+       		<View style={styles.row}>
+       		<Text>{item.key}
+       		</Text>
+       		
+       		<Text>{item.hoten}
+       		</Text>
+       		</View>
+       		}
+       		horizontal={false}
+       		numColumns={3}
+       		/> 
+       	
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row:{
+  	borderBottomWidth:1,
+  	padding:50,
+  	borderRightWidth:1
+  }
+});
--- screens/HomeScreen/mainView.js
+++ screens/HomeScreen/mainView.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
@@ -7,7 +7,7 @@ import {
   Image,
   StatusBar
 } from 'react-native';
-import { StackNavigator} from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import HomeScreen from './homeScreen';
 import SearchScreen from './searchScreen';
 import DetailScreen from './detailScreen';
@@ -15,100 +15,113 @@ import Header from '../../Header/header';
 import TabNavigator from 'react-native-tab-navigator';
 import ListProduct from './ProductInformation/listProduct';
 import ProductDetail from './ProductInformation/productDetail';
-export const ListScreen=StackNavigator({
-  Shop:{screen: HomeScreen},
-  Search:{screen: SearchScreen},
-  ListProduct:{screen: ListProduct},
-  ProductDetail:{screen: ProductDetail},
+export const ListScreen = StackNavigator({
+  Shop: { screen: HomeScreen },
+  Search: { screen: SearchScreen },
+  ListProduct: { screen: ListProduct },
+  ProductDetail: { screen: ProductDetail },
 },
   {
-    headerMode:'none'
+    headerMode: 'none'
   }
 );
 
-export default class MainView extends Component<{}> {
-  openTabView(){
-    const {open}=this.props;
+export default class MainView extends Component {
+  openTabView() {
+    const { open } = this.props;
     open();
   }
-  constructor(prop){
-		super(prop);
-		this.state={
-			selectedTab:'home'
-		}
-	}
-  render(){
-    return(
-      <View style={{flex:1}}>
-      <Header Open={this.openTabView.bind(this)}/>
-      <TabNavigator /*tabBarStyle={{backgroundColor:'#183544'}}*/>
-      <TabNavigator.Item
-        selected={this.state.selectedTab === 'home'}
-        title="Home"
-        titleStyle={{color:'black'}}
-        selectedTitleStyle={{color:'green'}}
-        renderIcon={() =>
-          <Image style={{width:20,height:20}}
-            source={require('../../images/home.png')}
-            resizeMode="stretch" />
-          }
-        renderSelectedIcon={() => <Image style={{width:20,height:20}}
-          source={require('../../images/home_red.png')}
-          resizeMode="stretch"
-          />}
-        onPress={() => this.setState({ selectedTab: 'home' })}>
-        {<ListScreen/>}
-      </TabNavigator.Item>
-      <TabNavigator.Item
-        selected={this.state.selectedTab === 'profile'}
-        title="Profile"
-        titleStyle={{color:'black'}}
-        renderIcon={() =>
-          <Image style={{width:20,height:20}}
-            source={require('../../images/profile.png')}
-            resizeMode="stretch" />
-          }
-          renderSelectedIcon={() => <Image style={{width:20,height:20}}
-          source={require('../../images/profile_red.png')}
-          resizeMode="stretch"
-          />}
-        onPress={() => this.setState({ selectedTab: 'profile' })}>
-        {<DetailScreen/>}
-      </TabNavigator.Item>
-      <TabNavigator.Item
-        selected={this.state.selectedTab === 'cart'}
-        title="Cart"
-        titleStyle={{color:'black'}}
-        renderIcon={() =>
-          <Image style={{width:20,height:20}}
-            source={require('../../images/cart.png')}
-            resizeMode="stretch" />
-          }
-          renderSelectedIcon={() => <Image style={{width:20,height:20}}
-          source={require('../../images/cart_red.png')}
-          resizeMode="stretch"
-          />}
-          badgeText="3"
-        onPress={() => this.setState({ selectedTab: 'cart' })}>
-        {<DetailScreen/>}
-      </TabNavigator.Item>
-      <TabNavigator.Item
-        selected={this.state.selectedTab === 'search'}
-        title="Search"
-        titleStyle={{color:'black'}}
-        renderIcon={() =>
-          <Image style={{width:20,height:20}}
-            source={require('../../images/search.png')}
-            resizeMode="stretch" />
-          }
-          renderSelectedIcon={() => <Image style={{width:20,height:20}}
-          source={require('../../images/search_red.png')}
-          resizeMode="stretch"
-          />}
-        onPress={() => this.setState({ selectedTab: 'search' })}>
-        {<DetailScreen/>}
-      </TabNavigator.Item>
-      </TabNavigator>
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'home',
+      type:[1,2,3,4],
+    }
+  }
+  componentDidMount() {
+    fetch("https://funnyshopjonah.000webhostapp.com")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState(
+         {type:responseJson.type}
+      );
+      console.log(responseJson.type);
+      console.log(this.state.type);
+    })
+    .catch((error) => { console.log(error)});
+  }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header Open={this.openTabView.bind(this)} />
+        <TabNavigator /*tabBarStyle={{backgroundColor:'#183544'}}*/>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'home'}
+            title="Home"
+            titleStyle={{ color: 'black' }}
+            selectedTitleStyle={{ color: 'green' }}
+            renderIcon={() =>
+              <Image style={{ width: 20, height: 20 }}
+                source={require('../../images/home.png')}
+                resizeMode="stretch" />
+            }
+            renderSelectedIcon={() => <Image style={{ width: 20, height: 20 }}
+              source={require('../../images/home_red.png')}
+              resizeMode="stretch"
+            />}
+            onPress={() => this.setState({ selectedTab: 'home' })}>
+            {<ListScreen />}
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            titleStyle={{ color: 'black' }}
+            renderIcon={() =>
+              <Image style={{ width: 20, height: 20 }}
+                source={require('../../images/profile.png')}
+                resizeMode="stretch" />
+            }
+            renderSelectedIcon={() => <Image style={{ width: 20, height: 20 }}
+              source={require('../../images/profile_red.png')}
+              resizeMode="stretch"
+            />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            {<DetailScreen />}
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'cart'}
+            title="Cart"
+            titleStyle={{ color: 'black' }}
+            renderIcon={() =>
+              <Image style={{ width: 20, height: 20 }}
+                source={require('../../images/cart.png')}
+                resizeMode="stretch" />
+            }
+            renderSelectedIcon={() => <Image style={{ width: 20, height: 20 }}
+              source={require('../../images/cart_red.png')}
+              resizeMode="stretch"
+            />}
+            badgeText="3"
+            onPress={() => this.setState({ selectedTab: 'cart' })}>
+            {<DetailScreen />}
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'search'}
+            title="Search"
+            titleStyle={{ color: 'black' }}
+            renderIcon={() =>
+              <Image style={{ width: 20, height: 20 }}
+                source={require('../../images/search.png')}
+                resizeMode="stretch" />
+            }
+            renderSelectedIcon={() => <Image style={{ width: 20, height: 20 }}
+              source={require('../../images/search_red.png')}
+              resizeMode="stretch"
+            />}
+            onPress={() => this.setState({ selectedTab: 'search' })}>
+            {<DetailScreen />}
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
@@ -118,7 +131,7 @@ const styles = StyleSheet.create({
   icon: {
     width: 18,
     height: 18,
-    padding:0,
-    margin:0
+    padding: 0,
+    margin: 0
   },
 });
